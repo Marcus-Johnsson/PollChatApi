d78448d83ae9a3584cf35a3cee779acb344ad683
Marcus <marcus.johnsson@campusnykoping.se>
2025-06-16 23:56:13 +0200

    a

--- PollChatApi/Program.cs
+++ PollChatApi/Program.cs
@@ -58,12 +58,11 @@ namespace PollChatApi
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            
                 app.MapOpenApi();
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
